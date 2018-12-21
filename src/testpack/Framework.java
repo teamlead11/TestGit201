0fdaa69bff4da75e394530caa2b894cbe34af98a
teamlead11 <automationlead11@gmail.com>
2018-12-21 09:25:21 +0530

    added framework class

---
+++ src/testpack/Framework.java
@@ -0,0 +1,14 @@
+package testpack;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		baseClassMethod();
+	}
+
+	public static void baseClassMethod() {
+
+		System.out.println("this is my base project");
+	}
+}
