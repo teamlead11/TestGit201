2b00b06d5bcabf3793b4f5c99d46d8a957be54c9
teamlead11 <automationlead11@gmail.com>
2018-12-21 09:31:27 +0530

    modifide base class

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -4,11 +4,11 @@ public class Framework {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		baseClassMethod();
 	}
 
 	public static void baseClassMethod() {
 
+		
 		System.out.println("this is my base project");
 	}
 }
