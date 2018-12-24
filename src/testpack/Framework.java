1833f63eecfb4d9fb26511f1c1f929a26d9289be
srilakshmi-malineni <srilakshmi529@gmail.com>
2018-12-24 16:40:37 +0530

    modified framework class

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -1,14 +1,31 @@
 package testpack;
 
+import java.util.Arrays;
+
 public class Framework {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
-	public static void baseClassMethod() {
-
 		
-		System.out.println("this is my base project");
+		Framework f=new Framework();
+		f.sortArraySortBySri();
+		
+		}
+
+	public void sortArraySortBySri() {
+	
+		int num[]= {10,5,30,20,15};
+		
+		System.out.println("Before Sorting:");
+		for(int i:num) {
+			System.out.println(i);
+		}
+		Arrays.sort(num);
+		System.out.println("After Sorting:");
+		for(int n:num) {
+			System.out.println(n);
+		}
+		
+		
+		
 	}
 }
