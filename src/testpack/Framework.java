162f3225bb2156d344ed661db0b297269315b1e4
JananiKaruna <jananikaruna3@gmail.com>
2018-12-24 23:30:39 +0530

    AddedSortArray

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -3,12 +3,23 @@ package testpack;
 public class Framework {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		baseClassMethod();
+		sortArrayJanani();
 	}
 
 	public static void baseClassMethod() {
-
-		
 		System.out.println("this is my base project");
+	}
+	public static void sortArrayJanani() {
+		int a[]= {38,12,80,5,45};
+		for (int i=0;i<a.length;i++) {
+			for (int j=i+1;j<a.length;j++) {
+				if(a[i]>a[j]) {
+					int temp = a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}System.out.print(a[i] + " ");
+		}
 	}
 }
