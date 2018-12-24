7b6432ce307d8111251f3c1fdbd333bdc775ee71
selenium246 <meenaraj246@gmail.com>
2018-12-24 18:51:48 +0530

    modified framework class

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -1,14 +1,30 @@
 package testpack;
 
-public class Framework {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
+public class Framework {
+	
+	public void sortArrayByMeena() {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(1);
+        al.add(10);
+        al.add(4);
+        al.add(6);
+        al.add(2);
+        System.out.println("Before List sort" +al);
+        Collections.sort(al);
+        System.out.println("After List sort" +al);
+        }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Framework a=new Framework();
+		a.sortArrayByMeena();
 	}
 
-	public static void baseClassMethod() {
+	 
 
 		
-		System.out.println("this is my base project");
-	}
+		
+	
 }
