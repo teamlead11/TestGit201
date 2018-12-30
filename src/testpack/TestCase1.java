2b50410fa512f87cbc2f1b56c886a95da0cff115
ABINAYASIVAKUMAR95 <sabinayakumar95@gmail.com>
2018-12-30 17:34:44 +0530

    add new branch

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -21,6 +21,9 @@ public class TestCase1 {
 	public static void add(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
+	public static void add(int a, int b, int c,int d) {
+		System.out.println(a + b + c +d);
+	}
 
 
 	public static void testCase2() {
