c737eb9ebbada1058d2c0d1bff75ef393113e925
ABINAYASIVAKUMAR95 <sabinayakumar95@gmail.com>
2018-12-30 14:10:26 +0530

    added methods

--- src/testpack/TestCase2.java
+++ src/testpack/TestCase2.java
@@ -11,11 +11,12 @@ public class TestCase2 {
 		System.out.println("this is my test case1");
 	}
 
-	public static void add(int a, int b, int c) {
-		System.out.println(a + b+c);
+	public static void add(int a, int b, int c,int d) {
+		System.out.println(a + b+c+d);
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
+		System.out.println(d);
 		
 	}
 }
